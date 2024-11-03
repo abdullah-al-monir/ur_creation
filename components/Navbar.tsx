d5bf9438cdc5dcc8a0d579e6c31d5a5855ce427d
MrColonThree <abdullahalmonir364@gmail.com>
2024-11-03 06:55:28 +0600

    Implemented next auth with github login functionality and created hero section in the homepage

---
+++ components/Navbar.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signIn, signOut } from "@/auth";
+
+const Navbar = async () => {
+  const session = await auth();
+  console.log(session);
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href={"/"}>
+          <Image
+            src="/logo.png"
+            alt="URCreation Logo"
+            width={144}
+            height={50}
+          />
+        </Link>
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Sign Out</button>
+              </form>
+              {/* <img
+                src={session?.user?.image}
+                alt="Profile picture"
+                className="rounded-full size-10"
+              /> */}
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <form
+                action={async () => {
+                  "use server";
+                  await signIn("github");
+                }}
+              >
+                <button type="submit">Signin with GitHub</button>
+              </form>
+            </>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
