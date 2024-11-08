4c63f99ce9a9ff39aa4b9f1d8d9f5990dcd1083c
MrColonThree <abdullahalmonir364@gmail.com>
2024-11-09 05:08:10 +0600

    Added search functionality and declared schema type and implemented some other functionalitites

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from "@/components/SearchForm";
-import { StartupCard } from "@/components/StartupCard";
+import { StartupCard, StartupTypeCard } from "@/components/StartupCard";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({
   searchParams,
@@ -7,18 +9,19 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 54,
-      author: { _id: 1, name: "Alex Lulu" },
-      _id: 1,
-      description: "This is a description",
-      image: "https://images.unsplash.com/photo-1527430253228-e93688616381",
-      category: "Robots",
-      title: "Tesla Robots",
-    },
-  ];
+  const params = { search: query || null };
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+  //   {
+  //     _createdAt: new Date(),
+  //     views: 54,
+  //     author: { _id: 1, name: "Alex Lulu" },
+  //     _id: 1,
+  //     description: "This is a description",
+  //     image: "https://images.unsplash.com/photo-1527430253228-e93688616381",
+  //     category: "Robots",
+  //     title: "Tesla Robots",
+  //   },
+  // ];
   return (
     <>
       <section className="blue_container">
@@ -35,16 +38,19 @@ export default async function Home({
 
       <section className="section_container">
         <p className="text-30-semibold">
-          {query ? `Search results for ${query}` : "All Startups"}
+          {query ? `Search results for "${query}"` : "All Startups"}
         </p>
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
-            posts.map((post) => <StartupCard key={post?._id} post={post} />)
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
           ) : (
             <p className="no-results">No startups found</p>
           )}
         </ul>
       </section>
+      <SanityLive />
     </>
   );
 }
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,18 +4,24 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
+import { Author } from "@/sanity/types";
+
+export type StartupTypeCard = Omit<StartupTypeCard, "author"> & {
+  author?: Author;
+};
 
 export const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
     _createdAt,
     views,
-    author: { _id: author_id, name },
+    author,
     category,
     image,
     title,
     _id,
     description,
   } = post;
+
   return (
     <li className="startup-card group">
       <div className="flex-between">
@@ -27,14 +33,14 @@ export const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </div>
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${author_id}`}>
-            <p className="text-10-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-10-medium line-clamp-1">{author?.name}</p>
           </Link>
           <Link href={`/startup/${_id}`}>
             <h3 className="text-20-semibold line-clamp-1">{title}</h3>
           </Link>
         </div>
-        <Link href={`/user/${author_id}`}>
+        <Link href={`/user/${author?._id}`}>
           <Image
             src="https://placehold.co/600x400"
             alt="placeholder"
@@ -49,7 +55,7 @@ export const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         <img src={image} alt="placeholder" className="startup-card_img" />
       </Link>
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/query=${category.toLowerCase()}`}>
+        <Link href={`/query=${category?.toLowerCase()}`}>
           <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup-card_btn">
